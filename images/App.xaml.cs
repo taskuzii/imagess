85a632338ed49eb24fa25e6fd2d28fdcf363e7ec
uzoma <uzoma@UZII>
2024-04-10 08:28:16 +0100

    Add project files.

---
+++ images/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace images
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ images/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace images
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
